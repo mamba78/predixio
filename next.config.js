c54b1779ae5b4be8aa9e6232348cb8074bb6f85b
Developer <st.bithas@gmail.com>
2025-12-05 04:02:03 +0000

    FINAL: clean next.config.js – no static export

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This single line FORCES dynamic mode – no static export
+  output: undefined,
+};
+
+module.exports = nextConfig;
